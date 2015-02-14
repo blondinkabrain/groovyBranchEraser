efbcfd7ae0c365943f9cee6b49b0986eb2ae221d
Alena Chistyakova <a.d.chistyakova@gmail.com>
2015-02-14 22:52:10 +0400

    branchCreator test2

--- src/branchCreator.groovy
+++ src/branchCreator.groovy
@@ -1,6 +1,10 @@
 /**
  * Created by Alenka on 14.02.2015.
  */
+/*import groovy.time.TimeCategory
+// Получение groovy-wslite для работы с Mantis API
+@Grab(group='com.github.groovy-wslite', module='groovy-wslite', version='0.7.2')
+import wslite.soap.**/
 if (args.size() < 2) {
     println "Usage: branchCreator repository-dir branchStartName"
     return
@@ -29,13 +33,23 @@ def executeShellCommand = { shellCommand, workingDir ->
 def executeGitCommand = { gitCommand ->
     executeShellCommand(gitCommand, repositoryDir)
 }
+// На всякий случай, проверяем, существует ли вообще репозиторий Git по переданному пути
+if (!repositoryDir.exists() || executeGitCommand('git rev-parse --git-dir').trim() != '.git') {
+    System.err.println "Folder ${repositoryDir} is not valid Git repository"
+    return
+}
 
-def goToTestBranch = executeGitCommand('git checkout -b test')
+// Скрипт нужно выполнять из ветки 'master'
+print executeGitCommand('git checkout master')
+executeGitCommand('git checkout -d test')
+executeGitCommand('git checkout -b test')
+
+def goToTestBranch = executeGitCommand('git checkout test')
 def createBranches= {param ->
 
     for (int i = 0; i < 10; i++) {
         print goToTestBranch
-        print executeGitCommand('git checkout -b $startBranch-$i')
+        print executeGitCommand('git checkout -b ${startBranch}-${i}')
     }
 }
-createBranches()
+//createBranches()
